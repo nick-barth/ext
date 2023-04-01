860fe870b34329c41c4299a9b8a4a69d9339e85b
nick-barth <barth.nicholas@gmail.com>
2023-04-01 17:45:25 +0200

    hello

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	const checkStatus = async () => {
+		const {
+			data: { session }
+		} = await browser.runtime.sendMessage({ action: 'getSession' });
+		console.log('session', session);
+	};
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
